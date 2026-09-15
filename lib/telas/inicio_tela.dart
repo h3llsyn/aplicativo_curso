44575b019f983db4f0f4619af9e4569c833b26fe
Lavínia <lavizsenai@gmail.com>
2026-09-15 12:41:54 -0300

    parte 7 da atividade finalizada

--- lib/telas/inicio_tela.dart
+++ lib/telas/inicio_tela.dart
@@ -1,72 +1,180 @@
 import 'package:flutter/material.dart';
 
-class InicioPage extends StatelessWidget{
+class InicioPage extends StatelessWidget {
   const InicioPage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ListView(
       padding: EdgeInsets.all(20),
       children: [
         Text(
           'Olá, estudante!',
-          style: Theme.of(context)
-          .textTheme
-          .headlineMedium
-          ?.copyWith(fontWeight: FontWeight.bold),
+          style: Theme.of(
+            context,
+          ).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold),
         ),
-
-        SizedBox(height: 8,),
-
-        Text(
-          'Continue aprendendo e evoluindo'
+        SizedBox(height: 8),
+        Text('Continue aprendendo e evoluindo'),
+        SizedBox(height: 24),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [
+                      const Color.fromARGB(255, 234, 114, 255),
+                      const Color.fromARGB(255, 255, 156, 25),
+                    ],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      '4',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Cursos iniciados',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(width: 12),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [const Color.fromARGB(255, 234, 114, 255),
+                      const Color.fromARGB(255, 255, 156, 25),],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      '1',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Cursos concluídos',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(width: 12),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [const Color.fromARGB(255, 234, 114, 255),
+                      const Color.fromARGB(255, 255, 156, 25),],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      '18',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Aulas concluídos',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-
-        SizedBox(height: 24,),
-
+        SizedBox(height: 24),
         Container(
           padding: EdgeInsets.all(24),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20),
             gradient: LinearGradient(
-              colors: [
-                Colors.redAccent,
-                Colors.pinkAccent
-              ],
+              colors: [const Color.fromARGB(255, 255, 99, 195), const Color.fromARGB(255, 209, 0, 70)],
             ),
             boxShadow: [
               BoxShadow(
                 color: Colors.black26,
                 blurRadius: 10,
-                offset: Offset(0, 5)
-              )
-            ]
+                offset: Offset(0, 5),
+              ),
+            ],
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Icon(
-                Icons.flutter_dash,
-                color: Colors.white,
-                size: 46,
-              ),
+              Icon(Icons.flutter_dash, color: Colors.white, size: 46),
               Text(
                 'Flutter Básico',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 24,
-                  fontWeight: FontWeight.bold
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               Text(
                 '8 de 12 aulas concluídas',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+                style: TextStyle(color: Colors.white),
               ),
             ],
-          )
-        )
+          ),
+        ),
       ],
     );
   }
